96b06526e2659fd1c1f3ee9384dc48dec1515069
machonky <martin@macrobert.com>
2024-05-20 17:36:32 +0800

    Permit overriding the Collection name

--- src/MacRobert.CodeGeneration.Contracts/ForeignCollectionContract.cs
+++ src/MacRobert.CodeGeneration.Contracts/ForeignCollectionContract.cs
@@ -3,10 +3,13 @@
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
 public class ForeignCollectionContractAttribute : ContractAttribute
 {
-    public ForeignCollectionContractAttribute(Type ForeignType)
+    public ForeignCollectionContractAttribute(Type ForeignType, string ForeignCollectionProperty)
     {
         this.ForeignType = ForeignType;
+        this.ForeignCollectionProperty = ForeignCollectionProperty;
     }
 
     public Type ForeignType { get; }
+
+    public string ForeignCollectionProperty { get; }
 }
