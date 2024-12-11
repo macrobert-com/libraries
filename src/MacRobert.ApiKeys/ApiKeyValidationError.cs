ea6e82ea1225da166a125a9aa3d10642b6b8eea1
machonky <martin@macrobert.com>
2024-12-11 14:10:37 +0800

    ApiKey - Refactor

---
+++ src/MacRobert.ApiKeys/ApiKeyValidationError.cs
@@ -0,0 +1,12 @@
+﻿using Macrobert.ResultPattern;
+
+namespace MacRobert.ApiKeys;
+
+public static class ApiKeyValidationError
+{
+    public static readonly Error IsNullOrWhiteSpaceError = new Error(nameof(ApiKeyValidationError) + "." + nameof(IsNullOrWhiteSpaceError), "ApiKey is null or whitespace");
+    public static readonly Error PatternMismatchError = new Error(nameof(ApiKeyValidationError) + "." + nameof(PatternMismatchError), "ApiKey does not match expected pattern");
+    public static readonly Error CreationDateParseError = new Error(nameof(ApiKeyValidationError) + "." + nameof(CreationDateParseError), "ApiKey CreationDate parse failure");
+    public static readonly Error CrcCheckError = new Error(nameof(ApiKeyValidationError) + "." + nameof(CrcCheckError), "ApiKey CRC check failure");
+    public static readonly Error HashValidationError = new Error(nameof(ApiKeyValidationError) + "." + nameof(HashValidationError), "ApiKey hash validation error");
+}
--- src/MacRobert.ApiKeys/ApiKeyValidator.cs
+++ src/MacRobert.ApiKeys/ApiKeyValidator.cs
@@ -61,12 +61,3 @@ public class ApiKeyValidator : ApiService, IApiKeyValidator
         return Result.Success();
     }
 }
-
-public static class ApiKeyValidationError
-{
-    public static readonly Error IsNullOrWhiteSpaceError = new Error(nameof(ApiKeyValidationError) + "." + nameof(IsNullOrWhiteSpaceError), "ApiKey is null or whitespace");
-    public static readonly Error PatternMismatchError = new Error(nameof(ApiKeyValidationError) + "." + nameof(PatternMismatchError), "ApiKey does not match expected pattern");
-    public static readonly Error CreationDateParseError = new Error(nameof(ApiKeyValidationError) + "." + nameof(CreationDateParseError), "ApiKey CreationDate parse failure");
-    public static readonly Error CrcCheckError = new Error(nameof(ApiKeyValidationError) + "." + nameof(CrcCheckError), "ApiKey CRC check failure");
-    public static readonly Error HashValidationError = new Error(nameof(ApiKeyValidationError) + "." + nameof(HashValidationError), "ApiKey hash validation error");
-}
